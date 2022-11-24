7abf787b54040f5dae20b49be834de3ac88fbee8
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-24 00:31:31 -0800

    initial commit of page fetcher assignment

---
+++ fetcher.js
@@ -0,0 +1,25 @@
+const url = process.argv[2];
+const path = process.argv[3];
+const fs = require('fs');
+const writefile = fs.writeFile;
+
+const request = require('request');
+
+request(url, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  writefile(path, body, (err) => {
+
+    if (err) {
+
+      console.log(err);
+    } else {
+
+      console.log('success!');
+    }
+  })
+});
+
+
+
