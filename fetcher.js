36145efe4eacb1f4b5cca8e35fb07fb2bdaf2bdf
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-27 15:46:15 -0800

    completed page fetcher assignment

--- fetcher.js
+++ fetcher.js
@@ -1,25 +1,22 @@
 const url = process.argv[2];
 const path = process.argv[3];
-const fs = require('fs');
+const fs = require("fs");
 const writefile = fs.writeFile;
 
-const request = require('request');
+const request = require("request");
 
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log("error:", error); // Print the error if one occurred
+  //console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+
   writefile(path, body, (err) => {
-
     if (err) {
-
       console.log(err);
-    } else {
-
-      console.log('success!');
+      return;
     }
-  })
+
+    const { size } = fs.statSync(path);
+
+    console.log(`Downloaded and saved ${size} bytes to ${path}`);
+  });
 });
-
-
-
